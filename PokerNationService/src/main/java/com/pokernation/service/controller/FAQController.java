0cef8e13a1f4f1310211436b6ce83f10ea4034d1
Baskar Felix <baskarf@gmail.com>
2017-11-01 01:01:05 +0530

    changes on faq controller

--- PokerNationService/src/main/java/com/pokernation/service/controller/FAQController.java
+++ PokerNationService/src/main/java/com/pokernation/service/controller/FAQController.java
@@ -5,126 +5,33 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by oneclicklabs.io
  */
 @RestController
-@RequestMapping("/poker")
+@RequestMapping("/webhook")
 public class FAQController {
 
 	// Logger instance
 	private static final Logger logger = Logger.getLogger(FAQController.class);
 
-	@RequestMapping(value = "/<add method name here>", method = RequestMethod.GET)
-	public String getSomething(@RequestParam(value = "request") String request,	@RequestParam(value = "version", required = false, defaultValue = "1") int version) {
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("Start getSomething");
-			logger.debug("data: '" + request + "'");
-		}
 
-		String response = null;
-
-		try {
-			switch (version) {
-			case 1:
-				if (logger.isDebugEnabled())
-					logger.debug("in version 1");
-				// TODO: add your business logic here
-				response = "Response from Spring RESTful Webservice : "+ request;
-
-				break;
-			default:
-				throw new Exception("Unsupported version: " + version);
-			}
-		} catch (Exception e) {
-			response = e.getMessage().toString();
-		}
-
-		if (logger.isDebugEnabled()) {
-			logger.debug("result: '" + response + "'");
-			logger.debug("End getSomething");
-		}
-		return response;
-	}
-
-	@RequestMapping(value = "/faq", method = RequestMethod.POST)
-	public String postSomething(@RequestBody String request) {
+	@RequestMapping( method = RequestMethod.POST)
+	public @ResponseBody WebhookResponse webhook(@RequestBody String request) {
 		
 		if (logger.isDebugEnabled()) {
 			logger.debug("Start postSomething");
 			logger.debug("data: '" + request + "'");
 		}
 
-		String response = "Hello!! Baskar!! ";
+		String response = "Baskar!! ";
+		logger.info("data: '" + request + "'");
 
 	
-		return response;
+		return new WebhookResponse("Hello! " + response, "Text " + response);
 	}
 
-	@RequestMapping(value = "/<add method name here>", method = RequestMethod.PUT)
-	public String putSomething(@RequestBody String request,@RequestParam(value = "version", required = false, defaultValue = "1") int version) {
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("Start putSomething");
-			logger.debug("data: '" + request + "'");
-		}
-
-		String response = null;
-
-		try {
-			switch (version) {
-			case 1:
-				if (logger.isDebugEnabled())
-					logger.debug("in version 1");
-				// TODO: add your business logic here
-				response = "Response from Spring RESTful Webservice : "+ request;
-
-				break;
-			default:
-				throw new Exception("Unsupported version: " + version);
-			}
-		} catch (Exception e) {
-			response = e.getMessage().toString();
-		}
-
-		if (logger.isDebugEnabled()) {
-			logger.debug("result: '" + response + "'");
-			logger.debug("End putSomething");
-		}
-		return response;
-	}
-
-	@RequestMapping(value = "/<add method name here>", method = RequestMethod.DELETE)
-	public void deleteSomething(@RequestBody String request,@RequestParam(value = "version", required = false, defaultValue = "1") int version) {
-		
-		if (logger.isDebugEnabled()) {
-			logger.debug("Start putSomething");
-			logger.debug("data: '" + request + "'");
-		}
-
-		String response = null;
-
-		try {
-			switch (version) {
-			case 1:
-				if (logger.isDebugEnabled())
-					logger.debug("in version 1");
-				// TODO: add your business logic here
-
-				break;
-			default:
-				throw new Exception("Unsupported version: " + version);
-			}
-		} catch (Exception e) {
-			logger.info(e.getMessage());
-		}
-
-		if (logger.isDebugEnabled()) {
-			logger.debug("result: '" + response + "'");
-			logger.debug("End putSomething");
-		}
-	}
 }
